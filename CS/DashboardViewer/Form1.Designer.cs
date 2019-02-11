e99c55c02ac4bd73894c73dfb3e1cea61305345b
SergeyNakhankov <sergey.nakhankov@devexpress.com>
2019-02-11 14:13:06 +0300

    add example content

---
+++ CS/DashboardViewer/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Viewer {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.dashboardViewer1 = new DevExpress.DashboardWin.DashboardViewer(this.components);
+            this.ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardViewer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dashboardViewer1
+            // 
+            this.dashboardViewer1.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(236)))), ((int)(((byte)(239)))));
+            this.dashboardViewer1.Appearance.Options.UseBackColor = true;
+            this.dashboardViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dashboardViewer1.Location = new System.Drawing.Point(0, 49);
+            this.dashboardViewer1.Name = "Viewer1";
+            this.dashboardViewer1.Size = new System.Drawing.Size(1190, 546);
+            this.dashboardViewer1.TabIndex = 0;
+            // 
+            // ribbonControl1
+            // 
+            this.ribbonControl1.ExpandCollapseItem.Id = 0;
+            this.ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbonControl1.ExpandCollapseItem});
+            this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
+            this.ribbonControl1.MaxItemId = 1;
+            this.ribbonControl1.Name = "ribbonControl1";
+            this.ribbonControl1.Size = new System.Drawing.Size(1190, 49);
+            // 
+            // Form1
+            // 
+            this.AllowFormGlass = DevExpress.Utils.DefaultBoolean.False;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1190, 595);
+            this.Controls.Add(this.dashboardViewer1);
+            this.Controls.Add(this.ribbonControl1);
+            this.Name = "Form1";
+            this.Ribbon = this.ribbonControl1;
+            this.Text = "Dashboard Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardViewer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.DashboardWin.DashboardViewer dashboardViewer1;
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl1;
+    }
+}
---
+++ CS/DashboardViewer/Form1.cs
@@ -0,0 +1,37 @@
+﻿using DevExpress.DashboardCommon.ViewerData;
+using DevExpress.DashboardWin;
+using DevExpress.XtraBars;
+using DevExpress.XtraCharts;
+using System.Linq;
+
+namespace Viewer {
+    public partial class Form1 : DevExpress.XtraBars.Ribbon.RibbonForm {
+        public Form1() {
+            InitializeComponent();
+
+            dashboardViewer1.DashboardItemControlUpdated += Viewer_DashboardItemControlUpdated;
+            dashboardViewer1.CalculateHiddenTotals = true;
+            dashboardViewer1.LoadDashboard(@"..\..\Dashboards\dashboard1.xml");
+        }
+
+        private void Viewer_DashboardItemControlUpdated(object sender, DevExpress.DashboardWin.DashboardItemControlEventArgs e) {
+            if (e.DashboardItemName == "pieDashboardItem1") {
+                var viewer = (DashboardViewer)sender;
+                var chart = e.ChartControl;
+
+                var data = viewer.GetItemData(e.DashboardItemName);
+                var measure = data.GetMeasures()[0];
+
+                foreach (Series series in chart.Series) {
+                    var axisPoint = series.Tag as AxisPoint;
+                    if (axisPoint != null) {
+                        var total = data.GetSlice(axisPoint).GetValue(measure).DisplayText;
+                        var view = series.View as PieSeriesView;
+                        if (view != null)
+                            view.Titles[0].Text = string.Format("{0} - {1}", series.Name, total);
+                    }
+                }
+            }
+        }
+    }
+}
