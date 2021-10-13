08a3849d2f9e008340bdfb1af8927f276e9a4488
DevExpressExampleBot <fakeEmail>
2021-10-13 14:06:41 +0300

    autogenerate VB

--- VB/TotalsInChartsExample/Dashboards/Dashboard1.Designer.vb
+++ VB/TotalsInChartsExample/Dashboards/Dashboard1.Designer.vb
@@ -1,126 +1,127 @@
-﻿Namespace TotalsInChartsExample.Dashboards
-	Partial Public Class Dashboard1
-		''' <summary> 
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+Namespace TotalsInChartsExample.Dashboards
 
-		''' <summary> 
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Dashboard1
 
-		#Region "Component Designer generated code"
+        ''' <summary> 
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary> 
-		''' Required method for Designer support - do not modify 
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Dim dimension1 As New DevExpress.DashboardCommon.Dimension()
-			Dim measure1 As New DevExpress.DashboardCommon.Measure()
-			Dim dimension2 As New DevExpress.DashboardCommon.Dimension()
-			Dim resources As New System.ComponentModel.ComponentResourceManager(GetType(Dashboard1))
-			Dim fieldInfo1 As New DevExpress.DataAccess.Excel.FieldInfo()
-			Dim fieldInfo2 As New DevExpress.DataAccess.Excel.FieldInfo()
-			Dim fieldInfo3 As New DevExpress.DataAccess.Excel.FieldInfo()
-			Dim fieldInfo4 As New DevExpress.DataAccess.Excel.FieldInfo()
-			Dim fieldInfo5 As New DevExpress.DataAccess.Excel.FieldInfo()
-			Dim fieldInfo6 As New DevExpress.DataAccess.Excel.FieldInfo()
-			Dim fieldInfo7 As New DevExpress.DataAccess.Excel.FieldInfo()
-			Dim fieldInfo8 As New DevExpress.DataAccess.Excel.FieldInfo()
-			Dim fieldInfo9 As New DevExpress.DataAccess.Excel.FieldInfo()
-			Dim excelWorksheetSettings1 As New DevExpress.DataAccess.Excel.ExcelWorksheetSettings()
-			Dim excelSourceOptions1 As New DevExpress.DataAccess.Excel.ExcelSourceOptions(excelWorksheetSettings1)
-			Dim dashboardLayoutGroup1 As New DevExpress.DashboardCommon.DashboardLayoutGroup()
-			Dim dashboardLayoutItem1 As New DevExpress.DashboardCommon.DashboardLayoutItem()
-			Me.pieDashboardItem1 = New DevExpress.DashboardCommon.PieDashboardItem()
-			Me.dashboardExcelDataSource1 = New DevExpress.DashboardCommon.DashboardExcelDataSource()
-			CType(Me.pieDashboardItem1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(dimension1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(measure1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(dimension2, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.dashboardExcelDataSource1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
-			' 
-			' pieDashboardItem1
-			' 
-			dimension1.DataMember = "Category"
-			Me.pieDashboardItem1.Arguments.AddRange(New DevExpress.DashboardCommon.Dimension() { dimension1})
-			Me.pieDashboardItem1.ComponentName = "pieDashboardItem1"
-			measure1.DataMember = "RevenueQTD (Sum)"
-			dimension2.DataMember = "State"
-			Me.pieDashboardItem1.DataItemRepository.Clear()
-			Me.pieDashboardItem1.DataItemRepository.Add(measure1, "DataItem1")
-			Me.pieDashboardItem1.DataItemRepository.Add(dimension2, "DataItem0")
-			Me.pieDashboardItem1.DataItemRepository.Add(dimension1, "DataItem2")
-			Me.pieDashboardItem1.DataSource = Me.dashboardExcelDataSource1
-			Me.pieDashboardItem1.InteractivityOptions.IgnoreMasterFilters = False
-			Me.pieDashboardItem1.Name = "Pies 1"
-			Me.pieDashboardItem1.SeriesDimensions.AddRange(New DevExpress.DashboardCommon.Dimension() { dimension2})
-			Me.pieDashboardItem1.ShowCaption = True
-			Me.pieDashboardItem1.Values.AddRange(New DevExpress.DashboardCommon.Measure() { measure1})
-			' 
-			' dashboardExcelDataSource1
-			' 
-			Me.dashboardExcelDataSource1.ComponentName = "dashboardExcelDataSource1"
-			Me.dashboardExcelDataSource1.FileName = "|DataDirectory|\Data\Sales.xlsx"
-			Me.dashboardExcelDataSource1.Name = "Excel Data Source 1"
-			Me.dashboardExcelDataSource1.ResultSchemaSerializable = resources.GetString("dashboardExcelDataSource1.ResultSchemaSerializable")
-			fieldInfo1.Name = "Category"
-			fieldInfo1.Type = GetType(String)
-			fieldInfo2.Name = "Product"
-			fieldInfo2.Type = GetType(String)
-			fieldInfo3.Name = "State"
-			fieldInfo3.Type = GetType(String)
-			fieldInfo4.Name = "UnitsSoldYTD (Sum)"
-			fieldInfo4.Type = GetType(Double)
-			fieldInfo5.Name = "UnitsSoldYTDTarget (Sum)"
-			fieldInfo5.Type = GetType(Double)
-			fieldInfo6.Name = "RevenueQTD (Sum)"
-			fieldInfo6.Type = GetType(Double)
-			fieldInfo7.Name = "RevenueQTDTarget (Sum)"
-			fieldInfo7.Type = GetType(Double)
-			fieldInfo8.Name = "RevenueYTD (Sum)"
-			fieldInfo8.Type = GetType(Double)
-			fieldInfo9.Name = "RevenueYTDTarget (Sum)"
-			fieldInfo9.Type = GetType(Double)
-			Me.dashboardExcelDataSource1.Schema.AddRange(New DevExpress.DataAccess.Excel.FieldInfo() { fieldInfo1, fieldInfo2, fieldInfo3, fieldInfo4, fieldInfo5, fieldInfo6, fieldInfo7, fieldInfo8, fieldInfo9})
-			excelWorksheetSettings1.CellRange = Nothing
-			excelWorksheetSettings1.WorksheetName = "Sheet1"
-			excelSourceOptions1.ImportSettings = excelWorksheetSettings1
-			Me.dashboardExcelDataSource1.SourceOptions = excelSourceOptions1
-			' 
-			' Dashboard1
-			' 
-			Me.CurrencyCultureName = Nothing
-			Me.DataSources.AddRange(New DevExpress.DashboardCommon.IDashboardDataSource() { Me.dashboardExcelDataSource1})
-			Me.Items.AddRange(New DevExpress.DashboardCommon.DashboardItem() { Me.pieDashboardItem1})
-			dashboardLayoutItem1.DashboardItem = Me.pieDashboardItem1
-			dashboardLayoutItem1.Weight = 42.646071188717258R
-			dashboardLayoutGroup1.ChildNodes.AddRange(New DevExpress.DashboardCommon.DashboardLayoutNode() { dashboardLayoutItem1})
-			dashboardLayoutGroup1.DashboardItem = Nothing
-			dashboardLayoutGroup1.Weight = 100R
-			Me.LayoutRoot = dashboardLayoutGroup1
-			Me.Title.Text = "Sales by State - Totals"
-			CType(dimension1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(measure1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(dimension2, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.pieDashboardItem1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.dashboardExcelDataSource1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+        ''' <summary> 
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+'#Region "Component Designer generated code"
+        ''' <summary> 
+        ''' Required method for Designer support - do not modify 
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Dim dimension1 As DevExpress.DashboardCommon.Dimension = New DevExpress.DashboardCommon.Dimension()
+            Dim measure1 As DevExpress.DashboardCommon.Measure = New DevExpress.DashboardCommon.Measure()
+            Dim dimension2 As DevExpress.DashboardCommon.Dimension = New DevExpress.DashboardCommon.Dimension()
+            Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(TotalsInChartsExample.Dashboards.Dashboard1))
+            Dim fieldInfo1 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo2 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo3 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo4 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo5 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo6 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo7 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo8 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo9 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim excelWorksheetSettings1 As DevExpress.DataAccess.Excel.ExcelWorksheetSettings = New DevExpress.DataAccess.Excel.ExcelWorksheetSettings()
+            Dim excelSourceOptions1 As DevExpress.DataAccess.Excel.ExcelSourceOptions = New DevExpress.DataAccess.Excel.ExcelSourceOptions(excelWorksheetSettings1)
+            Dim dashboardLayoutGroup1 As DevExpress.DashboardCommon.DashboardLayoutGroup = New DevExpress.DashboardCommon.DashboardLayoutGroup()
+            Dim dashboardLayoutItem1 As DevExpress.DashboardCommon.DashboardLayoutItem = New DevExpress.DashboardCommon.DashboardLayoutItem()
+            Me.pieDashboardItem1 = New DevExpress.DashboardCommon.PieDashboardItem()
+            Me.dashboardExcelDataSource1 = New DevExpress.DashboardCommon.DashboardExcelDataSource()
+            CType((Me.pieDashboardItem1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((dimension1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((measure1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((dimension2), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.dashboardExcelDataSource1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me), System.ComponentModel.ISupportInitialize).BeginInit()
+            ' 
+            ' pieDashboardItem1
+            ' 
+            dimension1.DataMember = "Category"
+            Me.pieDashboardItem1.Arguments.AddRange(New DevExpress.DashboardCommon.Dimension() {dimension1})
+            Me.pieDashboardItem1.ComponentName = "pieDashboardItem1"
+            measure1.DataMember = "RevenueQTD (Sum)"
+            dimension2.DataMember = "State"
+            Me.pieDashboardItem1.DataItemRepository.Clear()
+            Me.pieDashboardItem1.DataItemRepository.Add(measure1, "DataItem1")
+            Me.pieDashboardItem1.DataItemRepository.Add(dimension2, "DataItem0")
+            Me.pieDashboardItem1.DataItemRepository.Add(dimension1, "DataItem2")
+            Me.pieDashboardItem1.DataSource = Me.dashboardExcelDataSource1
+            Me.pieDashboardItem1.InteractivityOptions.IgnoreMasterFilters = False
+            Me.pieDashboardItem1.Name = "Pies 1"
+            Me.pieDashboardItem1.SeriesDimensions.AddRange(New DevExpress.DashboardCommon.Dimension() {dimension2})
+            Me.pieDashboardItem1.ShowCaption = True
+            Me.pieDashboardItem1.Values.AddRange(New DevExpress.DashboardCommon.Measure() {measure1})
+            ' 
+            ' dashboardExcelDataSource1
+            ' 
+            Me.dashboardExcelDataSource1.ComponentName = "dashboardExcelDataSource1"
+            Me.dashboardExcelDataSource1.FileName = "|DataDirectory|\Data\Sales.xlsx"
+            Me.dashboardExcelDataSource1.Name = "Excel Data Source 1"
+            Me.dashboardExcelDataSource1.ResultSchemaSerializable = resources.GetString("dashboardExcelDataSource1.ResultSchemaSerializable")
+            fieldInfo1.Name = "Category"
+            fieldInfo1.Type = GetType(String)
+            fieldInfo2.Name = "Product"
+            fieldInfo2.Type = GetType(String)
+            fieldInfo3.Name = "State"
+            fieldInfo3.Type = GetType(String)
+            fieldInfo4.Name = "UnitsSoldYTD (Sum)"
+            fieldInfo4.Type = GetType(Double)
+            fieldInfo5.Name = "UnitsSoldYTDTarget (Sum)"
+            fieldInfo5.Type = GetType(Double)
+            fieldInfo6.Name = "RevenueQTD (Sum)"
+            fieldInfo6.Type = GetType(Double)
+            fieldInfo7.Name = "RevenueQTDTarget (Sum)"
+            fieldInfo7.Type = GetType(Double)
+            fieldInfo8.Name = "RevenueYTD (Sum)"
+            fieldInfo8.Type = GetType(Double)
+            fieldInfo9.Name = "RevenueYTDTarget (Sum)"
+            fieldInfo9.Type = GetType(Double)
+            Me.dashboardExcelDataSource1.Schema.AddRange(New DevExpress.DataAccess.Excel.FieldInfo() {fieldInfo1, fieldInfo2, fieldInfo3, fieldInfo4, fieldInfo5, fieldInfo6, fieldInfo7, fieldInfo8, fieldInfo9})
+            excelWorksheetSettings1.CellRange = Nothing
+            excelWorksheetSettings1.WorksheetName = "Sheet1"
+            excelSourceOptions1.ImportSettings = excelWorksheetSettings1
+            Me.dashboardExcelDataSource1.SourceOptions = excelSourceOptions1
+            ' 
+            ' Dashboard1
+            ' 
+            Me.CurrencyCultureName = Nothing
+            Me.DataSources.AddRange(New DevExpress.DashboardCommon.IDashboardDataSource() {Me.dashboardExcelDataSource1})
+            Me.Items.AddRange(New DevExpress.DashboardCommon.DashboardItem() {Me.pieDashboardItem1})
+            dashboardLayoutItem1.DashboardItem = Me.pieDashboardItem1
+            dashboardLayoutItem1.Weight = 42.646071188717258R
+            dashboardLayoutGroup1.ChildNodes.AddRange(New DevExpress.DashboardCommon.DashboardLayoutNode() {dashboardLayoutItem1})
+            dashboardLayoutGroup1.DashboardItem = Nothing
+            dashboardLayoutGroup1.Weight = 100R
+            Me.LayoutRoot = dashboardLayoutGroup1
+            Me.Title.Text = "Sales by State - Totals"
+            CType((dimension1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((measure1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((dimension2), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.pieDashboardItem1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.dashboardExcelDataSource1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me), System.ComponentModel.ISupportInitialize).EndInit()
+        End Sub
 
-		Private pieDashboardItem1 As DevExpress.DashboardCommon.PieDashboardItem
-		Private dashboardExcelDataSource1 As DevExpress.DashboardCommon.DashboardExcelDataSource
-	End Class
+'#End Region
+        Private pieDashboardItem1 As DevExpress.DashboardCommon.PieDashboardItem
+
+        Private dashboardExcelDataSource1 As DevExpress.DashboardCommon.DashboardExcelDataSource
+    End Class
 End Namespace
--- VB/TotalsInChartsExample/Form1.vb
+++ VB/TotalsInChartsExample/Form1.vb
@@ -1,38 +1,34 @@
-﻿Imports DevExpress.DashboardCommon.ViewerData
+Imports DevExpress.DashboardCommon.ViewerData
 Imports DevExpress.DashboardWin
 Imports DevExpress.XtraCharts
 
 Namespace TotalsInChartsExample
-	Partial Public Class Form1
-		Inherits DevExpress.XtraEditors.XtraForm
 
-		Public Sub New()
-			InitializeComponent()
+    Public Partial Class Form1
+        Inherits DevExpress.XtraEditors.XtraForm
 
-			AddHandler dashboardViewer1.DashboardItemControlUpdated, AddressOf Viewer_DashboardItemControlUpdated
-			dashboardViewer1.CalculateHiddenTotals = True
-			dashboardViewer1.DashboardSource = GetType(Dashboards.Dashboard1)
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+            Me.dashboardViewer1.DashboardItemControlUpdated += AddressOf Viewer_DashboardItemControlUpdated
+            dashboardViewer1.CalculateHiddenTotals = True
+            dashboardViewer1.DashboardSource = GetType(Dashboards.Dashboard1)
+        End Sub
 
-		Private Sub Viewer_DashboardItemControlUpdated(ByVal sender As Object, ByVal e As DashboardItemControlEventArgs)
-			If e.DashboardItemName = "pieDashboardItem1" Then
-				Dim viewer = DirectCast(sender, DashboardViewer)
-				Dim chart = e.ChartControl
-
-				Dim data = viewer.GetItemData(e.DashboardItemName)
-				Dim measure = data.GetMeasures()(0)
-
-				For Each series As Series In chart.Series
-					Dim axisPoint = TryCast(series.Tag, AxisPoint)
-					If axisPoint IsNot Nothing Then
-						Dim total = data.GetSlice(axisPoint).GetValue(measure).DisplayText
-						Dim view = TryCast(series.View, PieSeriesView)
-						If view IsNot Nothing Then
-							view.Titles(0).Text = String.Format("{0} - {1}", series.Name, total)
-						End If
-					End If
-				Next series
-			End If
-		End Sub
-	End Class
+        Private Sub Viewer_DashboardItemControlUpdated(ByVal sender As Object, ByVal e As DashboardItemControlEventArgs)
+            If e.DashboardItemName Is "pieDashboardItem1" Then
+                Dim viewer = CType(sender, DashboardViewer)
+                Dim chart = e.ChartControl
+                Dim data = viewer.GetItemData(e.DashboardItemName)
+                Dim measure = data.GetMeasures()(0)
+                For Each series As Series In chart.Series
+                    Dim axisPoint = TryCast(series.Tag, AxisPoint)
+                    If axisPoint IsNot Nothing Then
+                        Dim total = data.GetSlice(axisPoint).GetValue(measure).DisplayText
+                        Dim view = TryCast(series.View, PieSeriesView)
+                        If view IsNot Nothing Then view.Titles(0).Text = String.Format("{0} - {1}", series.Name, total)
+                    End If
+                Next
+            End If
+        End Sub
+    End Class
 End Namespace
